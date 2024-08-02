ea1ce506429da8766e07b4bb016709f31f2887aa
Patricia Ruiz Diaz <pruizdiaz927@gmail.com>
2024-08-02 20:13:12 -0300

    Realizando modificaciones para que funcione con exportaciones e importaciones de tipo ES6 en una nueva rama para comparar codigos

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const path = require('path');
+import express from 'express'
+import path from'path'
 
-const startServer = (options) => {
+export const startServer = (options) => {
     const { port, public_path = 'public'} = options
 
     // console.log(port)
@@ -23,6 +23,3 @@ const startServer = (options) => {
 
 }
 
-module.exports= {
-    startServer
-}
